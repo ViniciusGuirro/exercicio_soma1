dc10c57af5aa4215161b09c91a8c9b197c165347
Vinicius Guirro <viniciuseduardoguirro@hotmail.com>
2021-11-27 15:00:35 -0300

    Projeto criado

---
+++ src/Exercicio.java
@@ -0,0 +1,22 @@
+public class Exercicio{
+	
+	public static void main(String[] args){
+		
+		int indice = 13;
+		int SOMA = 0;
+		int K = 0;
+	
+		while(K < indice){
+			K = K + 1;
+
+            SOMA = SOMA + K;
+			
+		}
+
+       System.out.println(SOMA);
+	
+	}
+	
+}
+
+   
